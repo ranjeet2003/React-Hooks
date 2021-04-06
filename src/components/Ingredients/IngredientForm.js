77cd95601e63bc2795baf441dd2cdffc5ca88ebd
Ranjeet <ranjeetgautam13032@gmail.com>
2021-04-06 11:20:42 +0530

    passing state to the components

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import Card from "../UI/Card";
-import "./IngredientForm.css";
+import Card from '../UI/Card';
+import './IngredientForm.css';
 
-const IngredientForm = React.memo((props) => {
-  const [inputState, setInputState] = useState({ title: "", amount: "" });
+const IngredientForm = React.memo(props => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
 
-  const submitHandler = (event) => {
+  const submitHandler = event => {
     event.preventDefault();
-    // ...
-    console.log(inputState);
+    props.onAddIngredient({ title: enteredTitle, amount: enteredAmount });
   };
 
   return (
@@ -21,13 +21,9 @@ const IngredientForm = React.memo((props) => {
             <input
               type="text"
               id="title"
-              value={inputState.title}
-              onChange={(event) => {
-                const newTitle = event.target.value;
-                setInputState((prevInputState) => ({
-                  title: newTitle,
-                  amount: prevInputState.amount,
-                }));
+              value={enteredTitle}
+              onChange={event => {
+                setEnteredTitle(event.target.value);
               }}
             />
           </div>
@@ -36,13 +32,9 @@ const IngredientForm = React.memo((props) => {
             <input
               type="number"
               id="amount"
-              value={inputState.amount}
-              onChange={(event) => {
-                const newAmount = event.target.value;
-                setInputState((prevInputState) => ({
-                  title: prevInputState.title,
-                  amount: newAmount,
-                }));
+              value={enteredAmount}
+              onChange={event => {
+                setEnteredAmount(event.target.value);
               }}
             />
           </div>
